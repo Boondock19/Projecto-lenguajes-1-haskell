e471fd19719eeed4d16c6a04d0c695beccdd2231
Boondock19 <josematiasgonzalez20@gmail.com>
2022-06-21 20:15:46 -0400

    Podemos ign este

--- main.hs
+++ main.hs
@@ -1,17 +1,29 @@
 import Data.List
 import System.Environment
 
+
+
+                
+printInt x = print            
+
 main = do
     args <- getArgs
     progName <- getProgName
     putStrLn "The arguments are :"
-    mapM putStrLn args
+    mapM_ putStrLn args
     putStrLn "The program name is:"
     putStrLn progName
     fileContent <- readFile "Palabras.txt"
     -- Lista de todas las palabras.
     let listOfWords = lines fileContent
-    print(listOfWords)
+    print listOfWords
+    let turno = 1 
+    sumTurno turno
     
+   
     
+
+
+
+
 
