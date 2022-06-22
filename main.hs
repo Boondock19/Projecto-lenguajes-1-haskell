8dee9bb39e0463bd068761ec65bf05e86d03f4fc
AnaSofiaSantosCerpa <51213022+AnaSofiaSantosCerpa@users.noreply.github.com>
2022-06-22 09:54:33 -0400

    Add files via upload

--- main.hs
+++ main.hs
@@ -1,21 +1,35 @@
 import Data.List
 import System.Environment
+import Data.List.Split
+
+contains :: Eq a => a -> [a] -> Bool
+contains = \elem -> \myList ->
+  case myList of
+    [] -> False 
+    x:xs | x == elem -> True 
+    _:xs -> contains elem xs
+
 
 initMenteMaestra :: Int -> String -> IO ()
 initMenteMaestra currentTurn randomWord = do
     if currentTurn > 6 then putStrLn ("Haz perdido , la palabra era " ++ randomWord )
     else do
-         putStrLn "Por favor ingresa una palabra: "
-         x <- getLine
-
+        putStrLn "Por favor ingresa una palabra: "
+        x <- getLine
+        let y = splitOn "" x
+        let z = drop 1 y
           
-         if  length x /= 5  
+        if  length x /= 5  
             then do
                 putStrLn "Has ingresado una palabra invalida"
                 initMenteMaestra currentTurn randomWord
-         else if x == randomWord 
+        else if contains "Ñ" z || contains "ñ" z
+            then do
+                putStrLn "Has ingresado una palabra invalida"
+                initMenteMaestra currentTurn randomWord 
+        else if x == randomWord 
             then putStrLn ("Haz ganado!, la palabra era " ++ randomWord )
-         else do
+        else do
             putStrLn "Haz ingresado una palabra valida!"
             initMenteMaestra (currentTurn + 1 ) randomWord
 
@@ -53,6 +67,5 @@ main = do
     -- else do
     --     let cont = cont + 1
     --     main
-    
 
     
