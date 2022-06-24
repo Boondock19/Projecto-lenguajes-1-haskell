5dc1a7c5099378bc262bd69127e9c91d6e0939c7
Boondock19 <josematiasgonzalez20@gmail.com>
2022-06-23 23:20:35 -0400

    Agregados comentarios

--- main.hs
+++ main.hs
@@ -5,6 +5,11 @@ import System.Random
 import Data.List.Split
 import Data.Char
 
+{--
+    Funcion que verifica si un string contiene el caracter especificado
+
+--}
+
 contains :: Eq a => a -> [a] -> Bool
 contains elem myList
   = case myList of
@@ -12,9 +17,23 @@ contains elem myList
       x : xs | x == elem -> True
       _ : xs -> contains elem xs
 
+{--
+    Funcion que transforma todas las letras de un string
+    en mayusculas.
+--}
+
 mayuscula :: String -> String
 mayuscula []     = []
 mayuscula (x:xs) = toUpper x : [toUpper x | x <- xs]
+
+{--
+    Funcion que recibe la palabra ingresada por el usuarios
+    la lista de letras que tiene T ancladas en el string de evaluacion
+    y el string de evaluacion de la palabra ingresada por el usuario y 
+    verifica que no existan V si existe una T anclada a la letra en otro 
+    lado de la palabra.Si existe un caso como el anterior, entonces 
+    divide el string, cambia la V por - y vuelve a unirla, se llama recursivamente.
+--}
 
 revisionV:: Int -> [[Char]] -> [[Char]] -> [Char] -> [Char]
 revisionV num userWord listOfBulls stringEvaluation = do
@@ -33,6 +52,13 @@ revisionV num userWord listOfBulls stringEvaluation = do
         stringEvaluation
     
 
+{-- 
+    Funcion que recibe la palabra ingresada por el usuario,
+    la palabra random generada por la computadora, una lista vacia
+    que sera la contenedora del string de evaluacion y otra
+    lista contenedora que sera de la letras que tengan T ancladas
+    a ellas en el string de evalacion.
+--}
     
 revisar :: Int -> [[Char]] -> [[Char]] -> [Char] -> [[Char]] -> [Char]
 revisar num x randomWord lista listOfBulls= do
@@ -63,12 +89,23 @@ revisar num x randomWord lista listOfBulls= do
                         
     else do
         revisionV 0 x listOfBulls lista
-        
+
+
+{--
+    Funcion encarga de inicializar el modo mente maestra del juego de
+    vacas y toros, se llama recursivamente hasta que el usuario ingrese
+    una palabra valida para poder se evaluada y se detiene luego del turno
+    6, indicandole al usuario que ha perdida y cual era la palabra random.
+
+--}       
 
 initMenteMaestra :: Int -> String -> IO ()
 initMenteMaestra currentTurn randomWord = do
+
     if currentTurn > 6 then putStrLn ("Haz perdido , la palabra era " ++ randomWord )
+
     else do
+
         putStrLn "Por favor ingresa una palabra: "
         x <- getLine
         let x2 = mayuscula x
@@ -80,22 +117,21 @@ initMenteMaestra currentTurn randomWord = do
           
         if  length x /= 5 || contains "Ñ" lchar
             then do
+
                 putStrLn "Has ingresado una palabra invalida"
                 initMenteMaestra currentTurn randomWord
-        else if x2 == randomWord 
+
+        else if x2 == randomWord
+
             then putStrLn ("Haz ganado!, la palabra era " ++ randomWord )
         else do
-            
+
             putStrLn (revisar 0 lchar lrW [] [[]])
-            -- let (x,_:ys) = splitAt 1 (revisar 0 lchar lrW [] [[]])
-            -- putStrLn ("Este es x: " ++ x)
-            -- putStrLn ("Este es ys: " ++ ys)
-            -- let stringArrglado = x ++ "-" ++ ys
-            -- putStrLn ("Este es el string arreglado: " ++ stringArrglado)
             initMenteMaestra (currentTurn + 1 ) randomWord
 
 
 main = do
+
     args <- getArgs
     progName <- getProgName
     putStrLn "The arguments are :"
@@ -104,15 +140,16 @@ main = do
     putStrLn progName
     fileContent <- readFile "Palabras.txt"
     -- Lista de todas las palabras.
+
     let listOfWords = lines fileContent
     let sizeOfList = length listOfWords
+
     -- Obtenemos una palabra aleatoria de la lista.
+
     index <- randomRIO (0, sizeOfList - 1)
     let randomWord =  listOfWords !! index
     
     print ("La palabra es : " ++ randomWord ++ " con index : " ++ show index)
-
-    --print(listOfWords)
 
     -- Lista de puntos de las palabras
     let word = "12345"
